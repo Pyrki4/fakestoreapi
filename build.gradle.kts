822adae6e651954c43bc87179b31ba220885c81e
Vladislav Ilyushkin <ilyushkin.vlad@mail.ru>
2025-02-10 17:48:09 +0700

    git init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.aeonbits.owner:owner:1.0.12") // конфигурация
+
+    implementation("io.rest-assured:rest-assured:5.4.0") // api запросы
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
